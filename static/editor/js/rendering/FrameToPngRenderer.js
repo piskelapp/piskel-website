778344c96b68069fe3b19ab6664108f88f8a3dbc
jdescottes <julian.descottes@gmail.com>
2013-08-10 16:10:22 +0200

    Split static files in a dedicated folder

---
+++ static/editor/js/rendering/FrameToPngRenderer.js
@@ -0,0 +1,11 @@
+(function () {
+  var ns = $.namespace("pskl.rendering");
+
+  ns.FrameToPngRenderer = function (frame) {
+  	this.frame = frame;
+  };
+
+	ns.FrameToPngRenderer.prototype.render = function () {
+
+	}
+});
