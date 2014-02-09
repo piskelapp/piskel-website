03c9fec9eb9d23fb7a3ea76f77e57739caad14e0
juliandescottes <julian.descottes@gmail.com>
2014-02-10 00:40:35 +0100

    rt

--- static/editor/piskel-boot.js
+++ static/editor/piskel-boot.js
@@ -1,10 +1,40 @@
 (function () {
 
+  window.onPiskelReady = function () {
+    var loadingMask = document.getElementById('loading-mask');
+    loadingMask.style.opacity = 0;
+    window.setTimeout(function () {loadingMask.parentNode.removeChild(loadingMask);}, 600)
+    pskl.app.init();
+    // cleanup
+    delete window.exports;
+    delete window.loadDebugScripts;
+    delete window.done;
+    delete window.onPiskelReady;
+  };
+
+  var prefixPath = function (path) {
+    if (window.pskl && window.pskl.appEngineToken_) {
+      return '../' + path;
+    } else {
+      return path;
+    }
+  };
+
   var loadScript = function (src, callback) {
+    src = prefixPath(src);
     var script = window.document.createElement('script');
     script.setAttribute('src',src);
     script.setAttribute('onload',callback);
     window.document.body.appendChild(script);
+  };
+
+  var loadStyle = function (src) {
+    src = prefixPath(src);
+    var link = document.createElement('link');
+    link.setAttribute('href', src);
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('type', 'text/css');
+    document.head.appendChild(link);
   };
 
   if (window.location.href.indexOf("debug") != -1) {
@@ -12,28 +42,34 @@
     var scriptIndex = 0;
     window.loadNextScript = function () {
       if (scriptIndex == window.exports.scripts.length) {
-        pskl.app.init();
-        // cleanup
-        delete window.exports;
-        delete window.loadDebugScripts;
-        delete window.done;
+        window.onPiskelReady();
       } else {
         loadScript(window.exports.scripts[scriptIndex], "loadNextScript()");
         scriptIndex ++;
       }
     };
     loadScript("piskel-script-list.js", "loadNextScript()");
+
+    window.loadStyles = function () {
+      var styles = window.exports.styles;
+      for (var i = 0 ; i < styles.length ; i++) {
+        loadStyle(styles[i]);
+      }
+    };
+    loadScript("piskel-style-list.js", "loadStyles()");
   } else {
     var script;
     if (window.location.href.indexOf("pack") != -1) {
-      script = "../build/piskel-packaged.js";
+      script = "build/piskel-packaged.js";
     } else {
-      script = "../build/piskel-packaged-min.js";
+      script = "build/piskel-packaged-min.js";
     }
+    loadStyle('build/piskel-style-packaged.css');
+
     var loaderInterval = window.setInterval(function () {
       if (document.querySelectorAll("[data-iframe-loader]").length === 0) {
         window.clearInterval(loaderInterval);
-        loadScript(script, "pskl.app.init()");
+        loadScript(script, "onPiskelReady()");
       } else {
         console.log("waiting for templates to load ....");
       }
