0e85ff6730245dd681f9f6245020a55f8d5a0a0c
Dávid Szabó <david.szabo97@gmail.com>
2016-08-21 18:45:54 +0200

    Add progress bar for piskel animated preview
    Brand new progress bar for animated preview. It's a smooth progress bar
    with seperators so it is easy to see when it will change frame.
    
    Reworked animation loop to use requestAnimationFrame.

--- static/js/piskel-animated-preview.js
+++ static/js/piskel-animated-preview.js
@@ -2,6 +2,9 @@
   // @require piskel-utils
   // @require piskel-image-scale
 
+  // Progres bar height in percentage
+  var PROGRESS_BAR_HEIGHT = 0.04;
+
   window.pskl = window.pskl || {};
   window.pskl.website = window.pskl.website || {};
 
@@ -20,6 +23,7 @@
 
   var resetPreview = function(imageInfo) {
     imageInfo.animationIndex = 0;
+    imageInfo.lastAnimationIndex = -1;
     var context = imageInfo.canvas.getContext('2d');
     context.clearRect(0, 0, imageInfo.canvas.width, imageInfo.canvas.height);
     context.drawImage(imageInfo.preview_canvas, 0, 0);
@@ -61,7 +65,7 @@
     var imageInfo = images[id];
 
     if (imageInfo.timer) {
-      window.clearTimeout(imageInfo.timer);
+      window.cancelAnimationFrame(imageInfo.timer);
     }
 
     // Default to 1 fps
@@ -69,24 +73,99 @@
       imageInfo.fps = 1;
     }
 
-    imageInfo.timer = window.setTimeout(function() {
+    if (imageInfo.animationIndex == 0 && imageInfo.lastAnimationIndex != imageInfo.animationIndex) {
+      imageInfo.startTime = Date.now();
+    }
+
+    imageInfo.lastAnimationIndex = imageInfo.animationIndex;
+
+    imageInfo.timer = window.requestAnimationFrame(function() {
+      var timePerFrame = 1000 / imageInfo.fps;
+      var fullAnimationTime = imageInfo.frames * timePerFrame;
+      if (imageInfo.progresBarHover) {
+        imageInfo.startTime = Date.now() - (timePerFrame * (imageInfo.progressBarHoverFrame));
+      }
+      var timeElapsedSinceStart = Date.now() - imageInfo.startTime;
+
       var w = imageInfo.width * imageInfo.zoom,
         h = imageInfo.height * imageInfo.zoom;
       var context = imageInfo.canvas.getContext('2d');
       context.clearRect(0, 0, imageInfo.canvas.width, imageInfo.canvas.height);
       context.drawImage(imageInfo.framesheet[imageInfo.animationIndex], 0, 0, w, h);
-      imageInfo.animationIndex = (imageInfo.animationIndex + 1) % imageInfo.framesheet.length;
+      imageInfo.animationIndex = Math.floor(timeElapsedSinceStart / timePerFrame) % imageInfo.frames;
+
+      // Crisp lines
+      context.save();
+      context.translate(0.5, 0.5);
+      context.lineWidth = 1;
+      // Background
+      var rectWidth = Math.round(imageInfo.canvas.width / imageInfo.frames);
+      var rectHeight = Math.ceil(imageInfo.canvas.height * PROGRESS_BAR_HEIGHT);
+
+      context.globalAlpha = 0.2;
+      context.fillStyle = '#039BE5';
+      context.fillRect(0, 0, imageInfo.canvas.width, rectHeight);
+
+      // Progress
+      context.globalAlpha = 0.9;
+      context.save();
+      context.shadowColor = '#01579B';
+      context.shadowBlur = 20;
+      context.shadowOffsetX = context.shadowOffsetY = 0;
+      if (imageInfo.progresBarHover) {
+        context.fillRect(0, 0, Math.ceil(imageInfo.canvas.width * (timeElapsedSinceStart / fullAnimationTime) + rectWidth / 2), rectHeight);
+      } else {
+        context.fillRect(0, 0, Math.ceil(imageInfo.canvas.width * (timeElapsedSinceStart / fullAnimationTime)), rectHeight);
+      }
+      context.restore();
+
+      // Bars
+      context.globalAlpha = 0.8;
+      context.strokeStyle = '#1A237E';
+      context.strokeRect(0, 0, imageInfo.canvas.width - 1, rectHeight);
+      for (var i = 1; i < imageInfo.frames; i++) {
+        context.beginPath();
+        context.moveTo(i * rectWidth, 0)
+        context.lineTo(i * rectWidth, rectHeight);
+        context.stroke();
+      }
+
+      context.restore();
+
       startAnimationTimer(id);
-    }, 1000 / imageInfo.fps);
+    });
   };
 
   var onCanvasOut = function(id) {
     var imageInfo = images[id];
-    imageInfo.hover = false;
-    window.clearTimeout(imageInfo.timer);
+    imageInfo.hover = imageInfo.progresBarHover = false;
+    window.cancelAnimationFrame(imageInfo.timer);
     if (imageInfo.framesheet) {
       resetPreview(imageInfo);
     }
+  };
+
+  var onCanvasMove = function(event, id) {
+    var imageInfo = images[id];
+    var rect = imageInfo.canvas.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    var y = event.clientY - rect.top;
+
+    // On progress bar
+    if (imageInfo.framesheet) {
+      if (y <= imageInfo.canvas.height * PROGRESS_BAR_HEIGHT) {
+        imageInfo.progresBarHover = true;
+        var frame = Math.floor(x / (imageInfo.canvas.width / imageInfo.frames));
+        imageInfo.progressBarHoverFrame = frame;
+      } else if (imageInfo.progresBarHover) {
+        imageInfo.progresBarHover = false;
+      }
+    }
+  };
+
+  var onCanvasOutAnimate = function(id) {
+    var imageInfo = images[id];
+    imageInfo.progresBarHover = false;
   };
 
   var __id = -1;
@@ -114,7 +193,10 @@
       canvas: canvas,
       zoom: zoom,
       animationIndex: 0,
-      hover: false
+      lastAnimationIndex: -1,
+      hover: false,
+      progresBarHover: false,
+      progressBarHoverFrame: 0
     };
 
     image.parentNode.replaceChild(canvas, image);
@@ -122,6 +204,11 @@
     if (imageInfo.frames > 1) {
       if (animate) {
           onCanvasOver("key" + __id, targetSize);
+          canvas.parentNode.addEventListener('mouseout', (function(id, targetSize) {
+          return function(event) {
+            onCanvasOutAnimate(id, targetSize);
+          };
+        })("key" + __id, targetSize));
       } else {
         // Targetting parentNode because the canvas is below an inset border
         canvas.parentNode.addEventListener('mouseover', (function(id, targetSize) {
@@ -136,6 +223,12 @@
           };
         })("key" + __id, targetSize));
       }
+
+      canvas.parentNode.addEventListener('mousemove', (function(id, targetSize) {
+        return function(event) {
+          onCanvasMove(event, id, targetSize);
+        };
+      })("key" + __id, targetSize));
     }
   };
 })();
