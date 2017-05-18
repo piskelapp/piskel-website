8101753b5164cdb67539c3f3b1218f83b0046f24
juliandescottes <julian.descottes@gmail.com>
2017-05-19 00:07:45 +0200

    merge user and user details when rendering user for view

--- models/user_details.py
+++ models/user_details.py
@@ -3,6 +3,9 @@ from google.appengine.ext import db
 
 class UserDetails(db.Model):
     user_id = db.IntegerProperty()
+    name = db.StringProperty()
+    localisation = db.StringProperty()
+    avatar_url = db.StringProperty()
     admin = db.BooleanProperty(default=False)
 
 
@@ -14,3 +17,15 @@ def get_by_userid(user_id):
         userdetails.put()
 
     return userdetails
+
+
+# Merge the user details and user object
+def get_user_for_view(user):
+    details = get_by_userid(user.key.id())
+    view_user = {
+        'name': details.name or user.name,
+        'localisation': details.localisation,
+        'avatar_url': details.avatar_url or user.avatar_url,
+        'admin': details.admin,
+    }
+    return view_user
